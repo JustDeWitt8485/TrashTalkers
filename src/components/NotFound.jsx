c6a2281d9377fe1784bebc7703a156ffb908c9be
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-07-19 17:35:57 -0500

    refined 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 function NotFound() {
 
    return (
-      <div>
+      <React.Fragment class="mainbox">
+         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;900&display=swap" rel="stylesheet"></link>
+         <script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
+         <link rel="stylesheet" href="./style.css" />
 
-         <h1> 404 notFound!</h1>
-
-      </div>
+         <div class="mainbox">
+            <div class="err">4</div>
+            <div class="far">0</div>
+            <div class="err2">4</div>
+            <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first
+            place?<p>Let's go <a href="/">home</a> and try from there.</p>
+            </div>
+         </div>
+      </React.Fragment>
    )
 }
 
