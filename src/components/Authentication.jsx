762b1a407b70585679056e2ee74ac0dba36c2358
leannjames <gracefulmonk93@yahoo.com>
2020-07-20 16:48:26 -0400

    Adds a border around the banner area in the authentication.css file

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -11,24 +11,27 @@ const Authentication = ({ loading }) => {
 
   return (
     <React.Fragment>
-    <h2>Are you in search of somewhere to vent to other likeminded individuals? Look no further!</h2>
-    <div className='button'>      
-      <Button 
-        href="/signin" 
-        variant="outline-info">
-          Sign In
-      </Button>
-      <Button href="/signup" variant="outline-info">Sign Up</Button>
- 
-      {/* {user ? <CurrentUser {...user} /> : <SignInAndSignUp />} */}
-      <Card className='about'>
-          <Card.Header>About The Creators</Card.Header>
-          <Card.Body>
-            The people who were responsible for creating this app were Arianna G Basha, Meagan Ramey, Tracy DeWitt, Chad Thompson, Joshua Luvera, and Leann James. We have put our hearts into this app in hopes of making it great!
-          </Card.Body>
-          <Card.Footer>We hope you enjoy using our app!</Card.Footer>
-        </Card>
-    </div>
+      <div className='banner'>
+        <h2>Are you in search of somewhere to vent to other likeminded individuals? Look no further!</h2>
+        {/* <img src="../images/download.jpeg" alt="People Talking" /> */}
+      </div>
+      <div className='button'>      
+          <Button 
+            href="/signin" 
+            variant="outline-info">
+              Sign In
+          </Button>
+          <Button href="/signup" variant="outline-info">Sign Up</Button>
+    
+          {/* {user ? <CurrentUser {...user} /> : <SignInAndSignUp />} */}
+          <Card className='about'>
+              <Card.Header>About The Creators</Card.Header>
+              <Card.Body>
+                The people who were responsible for creating this app were Arianna G Basha, Meagan Ramey, Tracy DeWitt, Chad Thompson, Joshua Luvera, and Leann James. We have put our hearts into this app in hopes of making it great!
+              </Card.Body>
+              <Card.Footer>We hope you enjoy using our app!</Card.Footer>
+            </Card>
+      </div>
     </React.Fragment>
   )
 };
