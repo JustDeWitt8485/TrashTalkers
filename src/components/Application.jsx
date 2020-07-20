ed5eb970ba073cef32498efb698a15ed9174dd79
Chad Thompson <f1rsthompson@gmail.com>
2020-07-20 09:43:56 -0400

    added background

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -23,25 +23,50 @@ const Application = () => {
     <main 
     className="Application" 
     style={{
-      background: "url(https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260) no-repeat center center fixed",
+      background: "url(https://i.pinimg.com/564x/93/74/13/93741395307093f31463dcb4a0ffff14.jpg)",
       backgroundSize:'cover',
       height: '100%',
       overflow: 'hidden'
       }}>
       <NavBar />
       <h1>Think Piece</h1>
-      {/* <Authentication /> */}
       <Switch>
-        <Route exact path="/" component={Authentication}/>
-        {/* <Route exact path="/" component={Posts}/> */}
-        <Route exact path="/posts" component={Posts}/>
-        <Route exact path="/profile" component={ ProfilePage } />
-        <Route exact path="/posts/:id" component={ PostPage } />
-        <Route exact path="/signout" component={ SignOut} />
-        <Route exact path="/signin" component={ SignIn } />
-        <Route exact path="/signup" component={ SignUp } />
-        <Route exact path="/edityourprofile" component={() => < UserProfile {...user}/>} />
-        <Route path="*" component={() => <NotFound />} />
+        <Route 
+        exact path="/" 
+        component={Authentication}
+        />
+        <Route 
+        exact path="/posts" 
+        component={Posts}
+        />
+        <Route 
+        exact path="/profile" 
+        component={ ProfilePage } 
+        />
+        <Route 
+        exact path="/posts/:id" 
+        component={ PostPage } 
+        />
+        <Route 
+        exact path="/signout" 
+        component={ SignOut} 
+        />
+        <Route 
+        exact path="/signin" 
+        component={ SignIn } 
+        />
+        <Route 
+        exact path="/signup" 
+        component={ SignUp } 
+        />
+        <Route 
+        exact path="/edityourprofile" 
+        component={() => < UserProfile {...user}/>} 
+        />
+        <Route 
+        path="*" 
+        component={() => <NotFound />} 
+        />
       </Switch>
       <div>
         <br/>
