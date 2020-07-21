9f82937ae8dfa626ae56a85bf2514d004d55cdbe
Meagan Ramey <meagan.ramey@yahoo.com>
2020-07-21 10:58:16 -0400

    Added support Component.

--- src/components/SupportPage.jsx
+++ src/components/SupportPage.jsx
@@ -1,1 +1,44 @@
 import React from 'react'
+import {Card} from 'react-bootstrap'
+
+const SupportPage = () => {
+    return (
+        <>
+        <div>
+            <Card>
+                <Card.Body>
+                <Card.Title>2 Minute Timeout</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+                <Card.Body>
+                <Card.Title>5 Minute Meditation</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+                <Card.Body>
+                <Card.Title>Talk to a friend</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+                <Card.Body>
+                <Card.Title>Move your body / 10 minute yoga</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+                <Card.Body>
+                <Card.Title>5 senses check-in</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+                <Card.Body>
+                <Card.Title>Laugh</Card.Title>
+                <Card.Text></Card.Text>
+                </Card.Body>
+                <br/>
+            </Card>
+        </div>
+        </>
+    )
+}
+export const SupportPage
